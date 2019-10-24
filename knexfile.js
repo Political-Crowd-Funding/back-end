bb5d6a234e30a2572580165dda64392883aa43da
dgbailey <bailey.dust@gmail.com>
2019-10-24 16:55:24 -0700

    Auth0 dependencies added, file structure changed

--- knexfile.js
+++ knexfile.js
@@ -1,4 +1,5 @@
 // Update with your config settings.
+require('dotenv').config();
 
 module.exports = {
 
@@ -6,9 +7,9 @@ module.exports = {
     client: 'pg',
     connection: {
       host: '127.0.0.1',
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
+      database: process.env.DB_LOCAL_DEV_NAME,
+      user:process.env.DB_LOCAL_UN,
+      password: process.env.DB_LOCAL_PW
     },
     pool: {
       min: 2,
@@ -22,13 +23,33 @@ module.exports = {
     }
   },
 
+  // production: {
+  //   client: 'pg',
+  //   connection: {
+  //     host: '127.0.0.1',
+  //     database: 'my_db',
+  //     user:     'username',
+  //     password: 'password'
+  //   },
+  //   pool: {
+  //     min: 2,
+  //     max: 10
+  //   },
+  //   migrations: {
+  //     directory: './data/migrations'
+  //   },
+  //   seeds: {
+  //     directory: './data/seeds'
+  //   }
+  // },
+
   testing: {
     client: 'pg',
     connection: {
       host: '127.0.0.1',
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
+      database: process.env.DB_LOCAL_TEST_NAME,
+      user:process.env.DB_LOCAL_UN,
+      password: process.env.DB_LOCAL_PW
     },
     pool: {
       min: 2,
