b846880c6077296a17e10c5ba891df7e2410c606
dgbailey <bailey.dust@gmail.com>
2019-11-07 13:50:49 -0800

    added most recent updates to schema based on nov 6th review

--- data/migrations/20191024170111_schema_v1.js
+++ data/migrations/20191024170111_schema_v1.js
@@ -10,29 +10,30 @@ exports.up = function(knex) {
   })
 
   
-
-   
-
-  .createTable('voters',function(tbl){
-    tbl.increments('voter_id')
+  .createTable('users',function(tbl){
+    tbl.increments('user_id')
+    
     tbl
-      .boolean('public_official')
-      .defaultTo(false)
-      .notNullable()
-    tbl
-      .string('voter_email',225)
+      .string('email',225)
     tbl
       .string('street_address',225)
-    tbl
-      .boolean('registered')
+    
     tbl
       .bigInteger('state_id')
       .unsigned()
       .notNullable()
       .references('state_id')
       .inTable('state')
-      .onUpdate('CASCADE')
       .onDelete('CASCADE')
+
+    tbl
+      .bigInteger('pub_official_id')
+      .unsigned()
+      .references('pub_official_id')
+      .inTable('public_official')
+      .onDelete('CASCADE')
+      .unique()//should this be unique due to a one to one relationship
+
   
   })
 
@@ -40,38 +41,12 @@ exports.up = function(knex) {
 
   .createTable('public_official',function(tbl){
     tbl.increments('pub_official_id')
-    tbl
-      .boolean('public_official')
-      .defaultTo(true)//unsure if this needs to be knex.value.true
-      .notNullable()
-    tbl
-      .string('first',225)
-    tbl
-      .string('last',225)
+
     tbl
       .string('phone',12)
 
     tbl
-      .string('email',225)
-
-    tbl
       .string('website',225)
-      .nullable()
-    
-    tbl
-    .string('street_address',225)
-
-
-
-    tbl
-      .bigInteger('state_id')
-      .unsigned()
-      .notNullable()
-      .references('state_id')
-      .inTable('state')
-      .onUpdate('CASCADE')
-      .onDelete('CASCADE')
-
 
   })
 
@@ -89,27 +64,28 @@ exports.up = function(knex) {
   .createTable('posts',function(tbl){
     tbl.increments('post_id')
     tbl
-      .bigInteger('voter_id')
+      .bigInteger('user_id')
       .unsigned()
       .nullable()
-      .references('voter_id')
-      .inTable('voters')
+      .references('user_id')
+      .inTable('users')
       .onDelete('CASCADE')
-      .onUpdate('CASCADE')
-    tbl
-      .bigInteger('pub_official_id')
-      .unsigned()
-      .nullable()
-      .references('pub_official_id')
-      .inTable('public_official')
-      .onDelete('CASCADE')
-      .onUpdate('CASCADE')
+ 
+    
 
     tbl
       .text('post_body')
     tbl
       .timestamp('created_at').defaultTo(knex.fn.now());
+
+    tbl
+      .bigInteger('vote_tally')
+      .defaultTo(0)
    
+    //likes
+    //transaction
+    //big int
+    //default to zero
 
    
 
@@ -118,8 +94,6 @@ exports.up = function(knex) {
   //bridge table for unique tags for each post
 
   .createTable('tags_bridge',function(tbl){
-    tbl
-      .increments('tag_bridge_id')
     
     tbl
       .bigInteger('tag_id')
@@ -128,7 +102,6 @@ exports.up = function(knex) {
       .references('tag_id')
       .inTable('tags')
       .onDelete('CASCADE')
-      .onUpdate('CASCADE')
     
     tbl
       .bigInteger('post_id')
@@ -137,14 +110,13 @@ exports.up = function(knex) {
       .references('post_id')
       .inTable('posts')
       .onDelete('CASCADE')
-      .onUpdate('CASCADE')
   })
 
-  //if we want a polling feture it would be good to be able to tie user identity to likes 
+  //if we want a polling feature it would be good to be able to tie user identity to likes 
   //hence the FK columns (or just to be able to get demographics on who liked a post, eventually)
 
-  .createTable('likes', function(tbl){
-    tbl.increments('like_id')
+  .createTable('vote_table', function(tbl){
+    // tbl.increments('like_id')
 
     tbl
       .bigInteger('post_id')
@@ -153,32 +125,19 @@ exports.up = function(knex) {
       .references('post_id')
       .inTable('posts')
       .onDelete('CASCADE')
-      .onUpdate('CASCADE')
-
+   
     tbl
-        .integer('like_count')
-        .defaultTo(1)
-        .nullable()
-    tbl
-        .bigInteger('voter_id')
+        .bigInteger('user_id')
         .nullable()
         .unsigned()
-        .references('voter_id')
-        .inTable('voters')
+        .references('user_id')
+        .inTable('users')
         .defaultTo(null)
+        .onDelete('CASCADE')
 
-    tbl
-      .bigInteger('politician_id')
-      .nullable()
-      .unsigned()
-      .references('politician_id')
-      .inTable('politicians')
-      .defaultTo(null)
-      .onDelete('CASCADE')
-      .onUpdate('CASCADE')
+    
 
-    tbl
-    .timestamp('created_at').defaultTo(knex.fn.now());
+    tbl.unique(["user_id","post_id"])
 
     
   })
@@ -197,7 +156,7 @@ exports.up = function(knex) {
       .references('post_id')
       .inTable('posts')
       .onDelete('CASCADE')
-      .onUpdate('CASCADE')
+
 
     tbl
     .timestamp('created_at').defaultTo(knex.fn.now());
@@ -206,26 +165,25 @@ exports.up = function(knex) {
       .bigInteger('user_id')
       .notNullable()
       .unsigned()
-      //need to see if this is the best approach here
-      //should this be a unique value?
-      //there could be duplicates so probably not
+      .references('user_id')
+      .inTable('users')
+     
 
   })
 
 
-  //tags bridge
   
 };
 
 exports.down = function(knex) {
   knex.schema
   .dropTableIfExists('state')
-  .dropTableIfExists('voters')
+  .dropTableIfExists('users')
   .dropTableIfExists('public_official')
   .dropTableIfExists('tags')
   .dropTableIfExists('tags_bridge')
   .dropTableIfExists('posts')
-  .dropTableIfExists('likes')
+  .dropTableIfExists('vote_table')
   .dropTableIfExists('comments')
 
 };
